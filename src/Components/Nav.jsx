d1eb88031d960f36b9835d35ded226460297950d
SEBASTIEN_ARTESI <SEBASTIEN.ARTESI@gmail.com>
2022-12-12 22:45:24 +0100

    Ajout du module React-router, ajout de 2 pages dans App, création de 3 composants home, beers, nav, le tout intégrer dans App

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./Nav.css"
+
+const Nav = () => {
+  return (
+    <>
+    <nav>
+      <img src="./assets/logo.jpeg" alt="logo marque" />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Beers">Beers</Link>
+        </li>
+      </ul>
+    </nav>
+    </>
+  );
+};
+
+export default Nav;
